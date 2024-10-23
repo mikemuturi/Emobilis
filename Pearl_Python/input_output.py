a33eeefb1a32e64ad4fdc4ba557fdb6b2b61916f
mikemuturi <mikeirungumuturi@gmail.com>
2024-10-23 13:26:41 -0400

    python starter course

---
+++ Pearl_Python/input_output.py
@@ -0,0 +1,11 @@
+myFistName = input("Enter your first name ")
+mySecondName = input("Enter your second name ")
+
+
+print(f"Your name is {myFistName} {mySecondName}")
+
+num1= int(input("Enter first number "))
+num2= int(input("Enter secon number "))
+
+
+print(f"Find product of {num1*num2}")
---
+++ Pearl_Python/intro.py
@@ -0,0 +1,21 @@
+print("Hello Mike")
+
+myName = "Mike Muturi"
+
+print(myName)
+
+print("Good Afternoon " + myName+","+"How are You doing?" )
+
+num1= 10
+num2=12
+
+
+print(f"The sum of {num1} and {num2} is {num2+num1}")
+
+print(f"The product of {num1} and {num2} is {num2*num1}")
+
+print(f"The division of {num1} and {num2} is {num2/num1}")
+
+print(f"The modular of {num1} and {num2} is {num2%num1}")
+
+print(f"The difference of {num1} and {num2} is {num2-num1}")
